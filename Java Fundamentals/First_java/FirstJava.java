fe5248def2987cc8fb25c91640d3adc8976b59d0
Yassine Chebbi <yassine.chebbi2023@gmail.com>
2023-07-31 13:38:01 +0100

    First_java

---
+++ Java Fundamentals/First_java/FirstJava.java
@@ -0,0 +1,9 @@
+public class FirstJava{
+    public static void main(String[] args) {
+           System.out.println("My name is Coding DOJO");
+    System.out.println("I am  years old");  
+    System.out.println("My hometown is Nurbank , CA");
+	}
+
+
+}

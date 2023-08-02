bbb59189fafbcb4744e043d2c1ce267c59872882
Yassine Chebbi <yassine.chebbi2023@gmail.com>
2023-08-02 16:29:20 +0100

    ignore

--- OOP_adv/CarAndDriver/src/com/yassine/java/Driver.java
+++ OOP_adv/CarAndDriver/src/com/yassine/java/Driver.java
@@ -1,5 +1,22 @@
 package com.yassine.java;
 
-public class Driver {
+public class Driver extends Car {
+	//CONSTRUCTOR_TO_GET_THE_CAR_CONSTRUCTOR
+	public Driver() {
+		super();
+	}
+//Methods
+    public void drive() {
+        
+       setGas(getGas()-1);
+    }
+
+    public void useBoosters() {
+    	setGas(getGas()-3);
+    }
+
+    public void refuel() {
+    	setGas(getGas()+2); 
+    }
 
 }

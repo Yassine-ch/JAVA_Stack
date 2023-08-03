487bbab757313396d1073a62b2d0d726b306cdf5
Yassine Chebbi <yassine.chebbi2023@gmail.com>
2023-08-03 09:00:22 +0100

    Car_andDriver

--- OOP_adv/CarAndDriver/src/com/yassine/java/CarAndDriver.java
+++ OOP_adv/CarAndDriver/src/com/yassine/java/CarAndDriver.java
@@ -1,5 +1,15 @@
 package com.yassine.java;
 
 public class CarAndDriver {
+	public static void main(String []args) {
+		Driver driver1 = new Driver();
+		Driver driver2 = new Driver();
+		driver1.status();
+		driver1.drive();
+		driver1.status();
+		driver2.refuel();
+		driver2.status();
+		
+	}
 
 }

af102cdc2de2d18427543ee2ab893e0a7f1226b6
Yassine Chebbi <yassine.chebbi2023@gmail.com>
2023-08-06 13:30:27 +0100

    counter

--- JavaSpring/Counter/src/main/java/com/yassine/Counter/controller/CounterController.java
+++ JavaSpring/Counter/src/main/java/com/yassine/Counter/controller/CounterController.java
@@ -1,5 +1,21 @@
 package com.yassine.Counter.controller;
 
-public class CounterController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
+@Controller
+public class CounterController {
+	@RequestMapping("/your_server")
+	public String index(HttpSession session, Model model) {
+		if(session.getAttribute("count") == null) {
+			session.setAttribute("count", 0);
+		} else {Integer currentCount = (Integer) session.getAttribute("count");
+			currentCount++;
+			session.setAttribute("count", currentCount);			
+		}
+		return "index.jsp";
+
+}
 }
--- JavaSpring/Counter/src/main/webapp/WEB-INF/index.jsp
+++ JavaSpring/Counter/src/main/webapp/WEB-INF/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,9 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+<body>
+	<h1 class="text-center mt-5">Welcome User!</h1>
+	<p class="text-center mt-5"><a href="/counter/">Back to counter</a></p>
+</body>
 </body>
 </html>
